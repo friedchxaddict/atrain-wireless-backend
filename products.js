61e13628b5495083dfce943d36aa3ebf9780455d
@adrianfontanilla <@adrianfontanilla>
2023-07-23 22:25:12 -0500

    intiial push for phone e-commerce backend

---
+++ products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+      "https://res.cloudinary.com/dykojtkh5/image/upload/v1689818223/onlineShop/iphone12pro_znc8zi.jpg",
+  },
+  {
+    id: 2,
+    name: "iPhone 12",
+    desc: "5.4-inch display",
+    price: 699,
+    image:
+      "https://res.cloudinary.com/dykojtkh5/image/upload/v1689818230/onlineShop/iphone12_jhe2g2.jpg",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/dykojtkh5/image/upload/v1689818234/onlineShop/galaxyS_m0klaw.png",
+  },
+];
+
+module.exports = products;
